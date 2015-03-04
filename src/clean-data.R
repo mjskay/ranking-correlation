3d1617b084e1ffb3fec524b08e3f2078cd3093bb
Matthew Kay <matthew.kay@gmail.com>
2015-03-04 00:02:12 -0800

    initial comparison of MLE models

--- src/clean-data.R
+++ src/clean-data.R
@@ -10,6 +10,10 @@ library(gamlss)
 #read data
 df = read.csv("data/master.csv")
 
+#clean up columns from original data
+df = df %>%
+    rename(r = rbase)
+
 #calculate values used to derive filtering conditions used in original paper
 df = df %>%
     group_by(visandsign) %>%
@@ -17,7 +21,7 @@ df = df %>%
     join(df)
 
 df = df %>%
-    group_by(visandsign, rbase, approach) %>%
+    group_by(visandsign, r, approach) %>%
     summarize(
         jnd_mad = mad(jnd),			#within-group median absolute deviation 
         jnd_median = median(jnd)
@@ -25,23 +29,23 @@ df = df %>%
     join(df)
 
 #filters used by original paper: when either is true, that data was excluded
-df$p_chance_cutoff = df$p_chance > .2
-df$mad_cutoff = abs(df$jnd - df$jnd_median) > 3 * df$jnd_mad
+df$p_chance_cutoff = df$p_chance > .2	#visandsigns with > 20% observations of jnd worse than chance
+df$mad_cutoff = abs(df$jnd - df$jnd_median) > 3 * df$jnd_mad	#observations > 3 mads from the median
 
 #adjusted r (TODO: ensure properly replicating old method)
 df = df %>%
-    group_by(visandsign, rbase) %>%
+    group_by(visandsign, r) %>%
     summarize(jnd_mean_within_vsr = mean(jnd)) %>%
     join(df) %>%
     mutate(
-        ra = rbase + ifelse(approach == "above", 0.5, -0.5) * jnd_mean_within_vsr
+        ra = r + ifelse(approach == "above", 0.5, -0.5) * jnd_mean_within_vsr
     )
 
 #censorship based on being above / below ceiling/floor , and chance
 df = mutate(df,
     censoring_threshold = ifelse(approach == "below", 
-                    pmin(rbase - .05, .4), 
-                    pmin(.95 - rbase, .4)),
+                    pmin(r - .05, .4), 
+                    pmin(.95 - r, .4)),
     not_censored = jnd < censoring_threshold,
     censored_jnd = pmin(jnd, censoring_threshold)
 )
